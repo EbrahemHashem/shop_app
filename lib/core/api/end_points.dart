5c47e2f8c4f27761d58d3e5fb90caf9e060055e1
Ebrahem <ebrahemhashem72@gmail.com>
2024-11-10 21:39:00 +0200

    add api,errors folders

---
+++ lib/core/api/end_points.dart
@@ -0,0 +1,20 @@
+class EndPoints {
+  static String baseUrl = "https://food-api-omega.vercel.app/api/v1/";
+  static String signIn = "user/signin";
+  static String signUp = "user/signup";
+}
+
+class ApiKey {
+  static String status = "status";
+  static String errorMessage = "ErrorMessage";
+  static String email = "email";
+  static String password = "password";
+  static String id = "id";
+  static String message = 'message';
+  static String token = 'token';
+  static String name = 'name';
+  static String phone = 'phone';
+  static String confirmPassword = 'confirmPassword';
+  static String location = 'location';
+  static String profilePic = 'profilePic';
+}
