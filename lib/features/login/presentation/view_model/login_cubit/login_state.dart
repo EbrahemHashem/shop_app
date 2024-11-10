5aa0d1947f8815cc672d0ff73bcfde263fbad58e
Ebrahem <ebrahemhashem72@gmail.com>
2024-11-10 21:34:50 +0200

    login ,register cubit

---
+++ lib/features/login/presentation/view_model/login_cubit/login_state.dart
@@ -0,0 +1,20 @@
+part of 'login_cubit.dart';
+
+sealed class LoginState extends Equatable {
+  const LoginState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class LoginInitial extends LoginState {}
+
+final class LoginLoadign extends LoginState {}
+
+final class LoginFailure extends LoginState {
+  final String errMessage;
+
+  const LoginFailure({required this.errMessage});
+}
+
+final class LoginSuccess extends LoginState {}
